3936108f315d0b716932f5086668940bcd37fdae
yue <viethai2372k2@gmail.com>
2023-02-19 00:28:38 +0700

    activity cong dong

---
+++ app/src/main/java/com/example/hb/CongDongActivity.java
@@ -0,0 +1,72 @@
+package com.example.hb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class CongDongActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cong_dong);
+
+        TextView txtDK = findViewById(R.id.textViewDangKy);
+        TextView txtDN = findViewById(R.id.textViewDangNhap);
+
+        txtDK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog(R.layout.custom_dialog_dangky);
+            }
+        });
+
+        txtDN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog(R.layout.custom_dialog_dangnhap);
+            }
+        });
+    }
+
+    private void openDialog(int layout) {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(layout);
+
+        Window window = dialog.getWindow();
+        if(window == null) {
+            return;
+        }
+
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        WindowManager.LayoutParams windowAttributes = window.getAttributes();
+        windowAttributes.gravity = Gravity.CENTER;
+        window.setAttributes(windowAttributes);
+
+        dialog.setCancelable(true);
+
+        dialog.show();
+
+        if(layout == R.layout.custom_dialog_dangnhap) {
+            TextView txtQuenMatKhau = findViewById(R.id.textViewQuenMatKhau);
+            txtQuenMatKhau.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialog.cancel();
+                    openDialog(R.layout.custom_dialog_quenmatkhau);
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/com/example/hb/MainActivity.java
+++ app/src/main/java/com/example/hb/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.hb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView mNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNavigationView = findViewById(R.id.bottom_nav);
+        mNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_congdong:
+                        startActivity(new Intent(MainActivity.this, CongDongActivity.class));
+                        break;
+                    case R.id.action_kesach:
+                        break;
+                    case R.id.action_khampha:
+                        break;
+                    case R.id.action_canhan:
+                        break;
+                    default:
+                }
+                return true;
+            }
+        });
+    }
+
+    private void setUpViewPager() {
+
     }
 }

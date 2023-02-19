01868cfa94a5329f488780a97ef8a454eb4a986a
Hoang <phh61002@gmail.com>
2023-02-19 15:55:54 +0700

    complete transitioning fragment

--- app/src/main/java/com/example/hb/MainActivity.java
+++ app/src/main/java/com/example/hb/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.hb;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -38,7 +42,31 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+        TextView library_ui = findViewById(R.id.action_home_library);
+        TextView history_ui = findViewById(R.id.action_history);
+
+        history_ui.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new ReadHistoryFragment());
+            }
+        });
+
+        library_ui.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new LibraryFragment());
+            }
+        });
     }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.nav_host_fragment_content_main,fragment);
+        fragmentTransaction.commit();
+    }
+
 
     private void setUpViewPager() {
 

b0d26a9d2e8c9ac983cdbccaadecaf8318e93676
Hoang <phh61002@gmail.com>
2023-02-22 17:26:11 +0700

    chap activity

---
+++ app/src/main/java/com/example/hb/Activities/ChapActivity.java
@@ -0,0 +1,16 @@
+package com.example.hb.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hb.R;
+
+public class ChapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chap);
+    }
+}
